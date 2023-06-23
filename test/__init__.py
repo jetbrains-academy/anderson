7c679729d0df78e2c2c89c2e3f6771f38aafb200
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-06-23 13:11:23 +0400

    feat: add the anderson test data folder

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,7 @@
 from anderson.utils import PROJECT_ROOT
 
 EXAMPLES_FOLDER = PROJECT_ROOT / 'examples'
+
+RESOURCES_FOLDER = PROJECT_ROOT / 'test' / 'resources'
+
+ANDERSON_TEST_DATA_FOLDER = RESOURCES_FOLDER / 'anderson'
