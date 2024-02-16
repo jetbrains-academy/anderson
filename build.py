3fe9852e8d2cb2688499768aea8636328331205a
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-02-16 17:32:42 +0400

    feat: add poetry

--- build.py
+++ build.py
@@ -1,25 +1,12 @@
 import os
-import re
 import stat
-from pathlib import Path
 from platform import libc_ver, machine, system
-from typing import List, Optional
+from typing import Optional
 from urllib.request import urlretrieve
-
-from setuptools import find_packages, setup
 
 from anderson.utils import AGG_PATH
 
-VERSION = '0.3.1'
 AGG_VERSION = '1.4.2'
-
-REQUIREMENTS_FILE = Path(__file__).parent / 'requirements.txt'
-
-
-def get_requires() -> List[str]:
-    with open(REQUIREMENTS_FILE) as requirements_file:
-        return [re.sub(r'(git\+.*egg=(.*))', r'\2 @ \1', line) for line in requirements_file.read().split('\n')]
-
 
 def get_agg_bin_name() -> Optional[str]:  # noqa: WPS231
     agg_bin_name = None
@@ -61,35 +48,3 @@ def download_agg_bin() -> None:
 
 
 download_agg_bin()
-
-
-setup(
-    name='anderson',
-    version=VERSION,
-    author='Ilya Vlasov',
-    author_email='ilyavlasov2011@gmail.com',
-    description='A tool for automatically recording a terminal session into animated GIF files',
-    long_description='file: README.md',
-    long_description_content_type='text/markdown',
-    url='https://github.com/GirZ0n/anderson',
-    license='Apache License 2.0',
-    license_files='LICENSE',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'License :: OSI Approved :: Apache Software License',
-        'Intended Audience :: Education',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
-        'Topic:: Utilities',
-    ],
-    python_requires='>=3.8, <4',
-    install_requires=get_requires(),
-    include_package_data=True,
-    package_data={'anderson': ['bin/*']},
-    packages=find_packages(),
-    entry_points={'console_scripts': ['anderson=anderson.main:main']},
-)
